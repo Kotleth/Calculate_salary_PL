4ba2b76fe04c2c46fba0b97990613534e9d57937
Kotleth <135366321+Kotleth@users.noreply.github.com>
2023-06-26 13:30:31 +0200

    Delete target/release/deps directory

--- target/release/deps/salary_pl-aac684f7a41811b7.d
+++
@@ -1,5 +0,0 @@
-/Users/przemyslaw/Desktop/Rust_files/projects/salary_calc/salary_pl/Calculate_salary_PL/target/release/deps/salary_pl-aac684f7a41811b7: src/main.rs
-
-/Users/przemyslaw/Desktop/Rust_files/projects/salary_calc/salary_pl/Calculate_salary_PL/target/release/deps/salary_pl-aac684f7a41811b7.d: src/main.rs
-
-src/main.rs:
