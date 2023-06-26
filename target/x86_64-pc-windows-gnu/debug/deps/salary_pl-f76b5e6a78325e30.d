199d4674b485e2e517fc9d688c4df075bd5c63a5
Kotleth <135366321+Kotleth@users.noreply.github.com>
2023-06-26 13:31:24 +0200

    Delete target/x86_64-pc-windows-gnu/debug/deps directory

--- target/x86_64-pc-windows-gnu/debug/deps/salary_pl-f76b5e6a78325e30.d
+++
@@ -1,5 +0,0 @@
-/Users/przemyslaw/Desktop/Rust_files/projects/salary_calc/salary_pl/Calculate_salary_PL/target/x86_64-pc-windows-gnu/debug/deps/salary_pl-f76b5e6a78325e30.exe: src/main.rs
-
-/Users/przemyslaw/Desktop/Rust_files/projects/salary_calc/salary_pl/Calculate_salary_PL/target/x86_64-pc-windows-gnu/debug/deps/salary_pl-f76b5e6a78325e30.d: src/main.rs
-
-src/main.rs:
